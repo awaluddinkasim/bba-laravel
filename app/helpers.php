b63a877dc6b3105d9b8ede6320feccfbfe98f063
Lab Komputer <labkomputer@uim-makassar.ac.id>
2024-05-26 17:46:29 +0800

    embed youtube function

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
 if (!function_exists('translate')) {
@@ -14,5 +15,24 @@ if (!function_exists('translate')) {
             'arab' => $result[0][0][0],
             'latin' => $result[0][1][2]
         ];
+    }
+}
+
+if (!function_exists('embedYoutube')) {
+    function embedYoutube($url): string
+    {
+        if (Str::contains($url, 'embed')) return "https://$url";
+
+        if (Str::contains($url, 'https://')) {
+            $url = explode('/', $url);
+
+            $result = "https://$url[2]/embed/" . substr($url[3], strpos($url[3], "=") + 1);
+        } else {
+            $url = explode('/', $url);
+
+            $result = "https://$url[0]/embed/" . substr($url[1], strpos($url[1], "=") + 1);
+        }
+
+        return $result;
     }
 }
